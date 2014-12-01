eb5045207ef1085a8b3e88be978d0032d8a1f72f
Y. T. Chung <zonyitoo@gmail.com>
2014-12-01 20:44:11 +0800

    add tracer

---
+++ Assets/Scripts/ChasingMonsterScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChasingMonsterScript : MonoBehaviour {
+
+	public Animator monAnim; 
+	public bool metHero = false;
+
+	// Use this for initialization
+	void Start () {
+		monAnim = GetComponent<Animator> ();
+//		rigidbody2D.velocity = new Vector2 (12, 0);
+	}
+
+	void OnCollisionEnter2D (Collision2D other) {
+		if (other.gameObject.tag == "Hero") {
+			print("Fuck you! ");
+			metHero = true;
+		} else if (other.gameObject.tag == "Monster") {
+			other.collider.enabled = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (metHero == true) {
+			rigidbody2D.velocity = new Vector2 (0, 0);
+			monAnim.SetTrigger("Stop");
+		} else {
+			rigidbody2D.velocity = new Vector2 (11.5f, 0);
+		}
+	}
+
+	void OnBecameInvisible () {
+		Destroy(this.gameObject);  
+	}
+
+}
