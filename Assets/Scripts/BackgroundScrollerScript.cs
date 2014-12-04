7659f2951eb1fede2325f604d87708735c0bc6c8
kylejan <765108938@qq.com>
2014-12-04 22:17:59 +0800

    ethan add skySprites and clouds

--- Assets/Scripts/BackgroundScrollerScript.cs
+++ Assets/Scripts/BackgroundScrollerScript.cs
@@ -13,18 +13,19 @@ namespace Kirigami {
 		public int triggerNum;
 
 		public GameObject bg_prefab;
-		public GameObject cloud_prefab;
-		public GameObject skyMonster_prefab;
+		public List<GameObject> cloud_prefabs;
+		public List<GameObject> skySprite_prefabs;
 		public List<GameObject> bgList = new List<GameObject>();
 		public List<GameObject> cloudList = new List<GameObject>();
 		public List<GameObject> skyMonsterList = new List<GameObject>();
 
 		void GenerateCloud(Vector3 pos) {
-			Random.seed = System.Guid.NewGuid().GetHashCode();
+
 			int number = Random.Range (0, 4);
 
 			while (number > 0) {
-				GameObject cloud = (GameObject)Instantiate(cloud_prefab);
+
+				GameObject cloud = (GameObject)Instantiate(cloud_prefabs[Random.Range(0, cloud_prefabs.Count)]);
 				cloud.transform.position = new Vector3(Random.Range(pos.x, pos.x + offset),
 				                                       Random.Range(cloud.transform.position.y, cloud.transform.position.y + maxCloudDeltaHeight),
 				                                       cloud.transform.position.z);
@@ -49,11 +50,11 @@ namespace Kirigami {
 		}
 
 		void GenerateSkyMonster(Vector3 pos) {
-			Random.seed = System.Guid.NewGuid().GetHashCode();
+
 			int number = Random.Range (0, 2);
 			
 			while (number > 0) {
-				GameObject skyMonster = (GameObject)Instantiate(skyMonster_prefab);
+				GameObject skyMonster = (GameObject)Instantiate(skySprite_prefabs[Random.Range(0, skySprite_prefabs.Count)]);
 				skyMonster.transform.position = new Vector3(Random.Range(pos.x, pos.x + offset),
 				                                       Random.Range(skyMonster.transform.position.y, skyMonster.transform.position.y + maxSkyMonsterDelataHeight),
 				                                       skyMonster.transform.position.z);
@@ -105,6 +106,8 @@ namespace Kirigami {
 		}
 
 		void Start() {
+			Random.seed = System.Guid.NewGuid().GetHashCode();
+
 			triggerNum = 0;
 			GameObject bg1 = (GameObject)Instantiate(bg_prefab);
 			Vector3 pos1 = new Vector3 (offset * 2, bg1.transform.position.y, bg1.transform.position.z);
